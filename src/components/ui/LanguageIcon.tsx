f5c139fae4db85d6a1fed866b409cf09eac3f9db
AhteshamAlvi <ahteshamalvi20@gmail.com>
2026-04-06 19:45:50 -0400

    border

--- src/components/ui/LanguageIcon.tsx
+++ src/components/ui/LanguageIcon.tsx
@@ -12,15 +12,17 @@ export default function LanguageIcon({ lang }: { lang: LanguageIconType }) {
 
   return (
     <div className="group relative flex items-center">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={src}
-        alt={lang.name}
-        width={20}
-        height={20}
-        className="h-5 w-5 rounded-[4px]"
-        loading="lazy"
-      />
+      <div className="rounded-md border border-border p-1 transition-all duration-200 hover:border-primary/50 hover:shadow-sm">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={src}
+          alt={lang.name}
+          width={20}
+          height={20}
+          className="h-5 w-5 rounded-[3px]"
+          loading="lazy"
+        />
+      </div>
       {/* Tooltip on hover */}
       <span className="pointer-events-none absolute -top-8 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-foreground px-2 py-1 text-xs text-background opacity-0 transition-opacity group-hover:opacity-100">
         {lang.name}
